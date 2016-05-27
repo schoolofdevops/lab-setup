2231e0026d5646ecee8b6f357e33e3e17dc48f1d
ashwini9860 <auchoudhari@gmail.com>
2016-05-27 16:47:40 +0530

    Update init.pp

--- puppet/puppet-code/modules/lab-v2/manifests/init.pp
+++ puppet/puppet-code/modules/lab-v2/manifests/init.pp
@@ -1,5 +1,5 @@
 
-class lab_v2 {
+class lab-v2 {
 
 file { 'C:\windows\tmp\Git-2.8.3-32-bit.exe':
   source => 'puppet:///modules/lab_v2/files/Git-2.8.3-32-bit.exe'
@@ -9,18 +9,18 @@ package { 'git':
   ensure => installed,
   source => 'C:\windows\tmp\Git-2.8.3-32-bit.exe'
   install_option => ['VERYSILENT'],
-  require => File['C:\windows\tmp\Git-2.8.3-32-bit.exe']
+  require => File['C:\windows\tmp\Git-2.8.3-32-bit.exe'],
 }
 
-file { 'C:\windows\tmp\':
-  source => 'puppet:///modules/lab_v2/files/'
+file { 'C:\windows\tmp\AtomSetup.exe':
+  source => 'puppet:///modules/lab_v2/files/AtomSetup.exe'
   source_permission => ignore,
 }
 package { 'atom':
   ensure => installed,
-  source => 'C:\windows\tmp\'
+  source => 'C:\windows\tmp\AtomSetup.exe'
   install_option => ['VERYSILENT'],
-  require => File['C:\windows\tmp\']
+  require => File['C:\windows\tmp\AtomSetup.exe'],
 }
 
 file { 'C:\windows\tmp\VirtualBox-5.0.20-106931-Win.exe':
@@ -31,18 +31,18 @@ package { 'virtualbox':
   ensure => installed,
   source => 'C:\windows\tmp\VirtualBox-5.0.20-106931-Win.exe'
   install_option => ['VERYSILENT'],
-  require => File['C:\windows\tmp\VirtualBox-5.0.20-106931-Win.exe']
+  require => File['C:\windows\tmp\VirtualBox-5.0.20-106931-Win.exe'],
 }
 
-file { 'C:\windows\tmp\':
-  source => 'puppet:///modules/lab_v2/files/'
+file { 'C:\windows\tmp\vagrant_1.8.1.msi':
+  source => 'puppet:///modules/lab_v2/files/vagrant_1.8.1.msi'
   source_permission => ignore,
 }
-package { 'git':
+package { 'vagrant':
   ensure => installed,
-  source => 'C:\windows\tmp\'
-  install_option => ['VERYSILENT'],
-  require => File['C:\windows\tmp\']
+  source => 'C:\windows\tmp\vagrant_1.8.1.msi'
+  install_option => ['INSTALLDIR=C:\windows\tmp\vagrant_1.8.1.msi'],
+  require => File['C:\windows\tmp\vagrant_1.8.1.msi'],
 }
 
 }
