7eedcff0c3ebdea92ff2445cf30757d7f739b60b
Vijayboopathy <vijayboopathy.e@gmail.com>
2018-02-23 21:59:51 +0530

    changed node1 as nfs server

---
+++ kubernetes/vagrant-kube-cluster/setup-nfs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+    sudo mkfs.ext4 /dev/sdb
+    sudo apt-get update
+    sudo apt-get install nfs-kernel-server -y
+    sudo mkdir -p /var/nfs/kubernetes
+    sudo chown nobody:nogroup /var/nfs/kubernetes
+    sudo chmod 666  /etc/exports
+sudo cat > /etc/exports <<EOF
+/var/nfs/kubernetes    192.168.12.0/24(rw,sync,no_subtree_check)
+EOF
+    sudo chmod 644 /etc/exports
+    sudo systemctl restart nfs-kernel-server
+    #sudo systemctl disable ufw
